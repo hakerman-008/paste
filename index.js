55623d99c57265c8096ac0931fe2c0b3e7655abb
kshitiz-008 <142625808+kshitiz-008@users.noreply.github.com>
2024-02-09 14:17:56 +0545

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,30 @@
-const express = require('express');
-const app = express();
-
-app.get('/create', (req, res) => {
+app.get('/gist', async (req, res) => {
     try {
-        const userText = req.query.paste;
+        const userContent = req.query.content;
 
-        if (!userText) {
-            return res.status(400).json({ error: 'User text is required' });
+        if (!userContent) {
+            return res.status(400).json({ error: 'User content is required' });
         }
 
-       
-        const encodedText = encodeURIComponent(userText);
-        const link = `https://paste-kshitiz.onrender.com/paste/${encodedText}`;
+        const response = await octokit.request('POST /gists', {
+            description: 'Gist created through API',
+            public: false,
+            files: {
+                'content.txt': {
+                    content: userContent
+                }
+            },
+            headers: {
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        });
 
-        return res.json({ link });
+      
+        const rawUrl = response.data.files['content.txt'].raw_url;
+
+        return res.json({ rawUrl });
     } catch (error) {
         console.error('Error:', error);
         return res.status(500).json({ error: 'Internal server error' });
     }
-});
-
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
 });
